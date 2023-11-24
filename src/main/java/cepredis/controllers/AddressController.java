f4ff148b99f2206c05e7045952936e2e2e17af0e
thiagosnx <thiagosantanacq@gmail.com>
2023-11-24 13:57:49 -0300

    feat: transfer to service camade

--- src/main/java/cepredis/controllers/AddressController.java
+++ src/main/java/cepredis/controllers/AddressController.java
@@ -5,7 +5,9 @@ import cepredis.request.RequestAddress;
 import cepredis.service.AddressService;
 import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,38 +26,16 @@ import java.util.concurrent.TimeUnit;
 public class AddressController {
 
     @Autowired
-    private RedisTemplate<String, Address> redisTemplate;
+    private AddressService addressService;
 
 
     @PostMapping
-    public ResponseEntity<Address> saveAddress(@RequestBody RequestAddress requestAddress) throws Exception {
-        String key = "address:" + requestAddress.cep();
-        Address newAddress = new Address(requestAddress);
-        URL url = new URL("https://viacep.com.br/ws/"+requestAddress.cep()+"/json/");
-        URLConnection connection = url.openConnection();
-        InputStream is = connection.getInputStream();
-        BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
-
-        String cep = "";
-        StringBuilder jsonCep = new StringBuilder();
-
-        while((cep = br.readLine()) != null){
-            jsonCep.append(cep);
-        }
-
-        Address addressAux = new Gson().fromJson(jsonCep.toString(), Address.class);
-
-        newAddress.setCep(addressAux.getCep());
-        newAddress.setLogradouro(addressAux.getLogradouro());
-        newAddress.setComplemento(addressAux.getComplemento());
-        newAddress.setBairro(addressAux.getBairro());
-        newAddress.setLocalidade(addressAux.getLocalidade());
-        newAddress.setUf(addressAux.getUf());
-
-        redisTemplate.opsForValue().set(key, newAddress);
-        redisTemplate.expire(key, 300, TimeUnit.SECONDS);
-
-        return ResponseEntity.ok(newAddress);
+    public ResponseEntity<Address> createAddress(@RequestBody RequestAddress requestAddress) throws Exception {
+        Address newAddress = this.addressService.createAddress(requestAddress);
+        return new ResponseEntity<>(newAddress, HttpStatus.OK);
     }
+
+
+
 
 }
--- src/main/java/cepredis/service/AddressService.java
+++ src/main/java/cepredis/service/AddressService.java
@@ -3,14 +3,54 @@ package cepredis.service;
 
 import cepredis.models.Address;
 import cepredis.request.RequestAddress;
+import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.concurrent.TimeUnit;
 
 @Service
 public class AddressService {
+    @Autowired
+    private RedisTemplate<String, Address> redisTemplate;
 
+    public Address createAddress(@RequestBody RequestAddress requestAddress) throws Exception {
+        String key = "address:" + requestAddress.cep();
+        Address newAddress = new Address(requestAddress);
+        URL url = new URL("https://viacep.com.br/ws/"+requestAddress.cep()+"/json/");
+        URLConnection connection = url.openConnection();
+        InputStream is = connection.getInputStream();
+        BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+
+        String cep = "";
+        StringBuilder jsonCep = new StringBuilder();
+
+        while((cep = br.readLine()) != null){
+            jsonCep.append(cep);
+        }
+
+        Address addressAux = new Gson().fromJson(jsonCep.toString(), Address.class);
+
+        newAddress.setCep(addressAux.getCep());
+        newAddress.setLogradouro(addressAux.getLogradouro());
+        newAddress.setComplemento(addressAux.getComplemento());
+        newAddress.setBairro(addressAux.getBairro());
+        newAddress.setLocalidade(addressAux.getLocalidade());
+        newAddress.setUf(addressAux.getUf());
+
+        redisTemplate.opsForValue().set(key, newAddress);
+        redisTemplate.expire(key, 300, TimeUnit.SECONDS);
+
+        return newAddress;
+
+    }
 }
